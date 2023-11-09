07f957bdef9c4a67ef4a088e5eccc43d76d4a358
Pedro Leonardo <pedroleon.pc@hotmail.com>
2023-11-09 14:13:38 -0300

    Add Class Dogs and Object;

---
+++ hello-world/src/Animals/Dogs.java
@@ -0,0 +1,9 @@
+package Animals;
+
+public class Dogs {
+    public String name;
+    public String color;
+    public int height;
+    public double peso;
+
+}
--- hello-world/src/Main.java
+++ hello-world/src/Main.java
@@ -1,11 +1,22 @@
+import Animals.Dogs;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.TextStyle;
 import  java.util.Locale;
 
 public class Main {
+    public  static  void main(String[] args){
+        Dogs dog1 = new Dogs();
 
-    public static void lace(String[] args) {
+        dog1.name = "Nik";
+        dog1.color = "Brown";
+        dog1.height = 80;
+        dog1.peso = 20.4;
+
+    }
+
+    public static void dates() {
 
         String name = "Dav";
         LocalDate time = LocalDate.now();
@@ -16,7 +27,7 @@ public class Main {
         System.out.printf("Hello, i'm %s. and now is %s, in day: %s.", name, hournow.getHour(), dayofweek);
     }
 
-    public static void ma(String[] args){
+    public static void loops(){
         int index = 5;
         for(int i = 1; i<=10;i++){
             System.out.println(index + "x" + i + "=" + index * i);
@@ -28,7 +39,7 @@ public class Main {
 //        }
     }
 
-    public static void main(String[] args){
+    public static void vetores(){
         int[] numbers = {25,1,3,5,7};
         int maior = numbers[0];
         int menor = numbers[0];
@@ -36,8 +47,11 @@ public class Main {
         for(int i = 0; i < numbers.length; i++){
             if (numbers[i] > maior){
                 maior = numbers[i];
+            } else if (numbers[i] < menor) {
+                menor = numbers[i];
             }
         }
         System.out.println(maior);
+        System.out.println(menor);
     }
 }
